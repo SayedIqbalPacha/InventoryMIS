ffb353c9e0edba2759d137a062068916eaf788f5
sayediqbal <iqbalsadat534@gmail.com>
2026-09-01 11:35:44 +0430

    basic setup of frontend

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,3 +1,4 @@
+import Button from "./services/ui/Button"
 function App() {
 
   return (
@@ -10,6 +11,9 @@ function App() {
 function Hello(){
   return <div>
      <h1 className="text-yellow-400 font-semibold text-center text-[23px] mt-[5rem]">hello world</h1>
+     <button className="button">Click-Here</button>
+     <br></br>
+     <Button>new button</Button>
   </div>
 }
 export default App;
